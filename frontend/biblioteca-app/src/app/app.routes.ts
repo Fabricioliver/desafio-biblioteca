cd888dfb2dfbf3631bd7de09a6eca13f98a84a09
re046407 <fabricio.taguatinga@qintess.com>
2025-08-22 19:59:07 -0300

    feat(frontend): ajustes em app.component, app.config e rotas

--- frontend/biblioteca-app/src/app/app.routes.ts
+++ frontend/biblioteca-app/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { GenerosPage } from './pages/generos.page';
+import { AutoresPage } from './pages/autores.page';
+import { LivrosPage } from './pages/livros.page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'generos' },
+  { path: 'generos', component: GenerosPage },
+  { path: 'autores', component: AutoresPage },
+  { path: 'livros', component: LivrosPage },
+  { path: '**', redirectTo: 'generos' },
+];
