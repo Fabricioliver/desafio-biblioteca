9d7a80ce7306ae9e71fe1b7264b8109200aad244
re046407 <fabricio.taguatinga@qintess.com>
2025-08-22 21:46:17 -0300

    Fex: merge commit

--- backend/tests/Biblioteca.Tests/UnitTest1.cs
+++ backend/tests/Biblioteca.Tests/UnitTest1.cs
@@ -1,10 +1,11 @@
-﻿namespace Biblioteca.Tests;
+﻿using Xunit;
+using FluentAssertions;
 
 public class UnitTest1
 {
     [Fact]
     public void Test1()
     {
-
+        true.Should().BeTrue();
     }
 }
